f81efa7ed9df41703b4e4341d0bf341801650e7a
Tobias Skarhed <dehrax@users.noreply.github.com>
2017-09-28 16:13:12 +0200

    Calculate slope and angle at point on circle

--- MATLAB/bikewheel.m
+++ MATLAB/bikewheel.m
@@ -1,23 +1,43 @@
 function bikewheel(radius, v, angle)
 %Takes the radius and simulation and stuff... Look ot the goddamn
 %assignment
-%   Detailed explanation goes here
+%   Detailed explanation goes here gvolllll00
 
-%drawCircle(radius, [0 radius]);
+center = [radius radius];
+
+drawCircle(radius, center);
 hold on;
-for r = linspace(2*pi, 200*pi, 10000)
-points = linspace(r-2*pi, r, 12);
 
-[x y] = getCirclePoint(radius, points, [0 radius]);
+points = linspace(0, 2*pi);
+
+[x, y] = getCirclePoint(radius, points, center);
+
+index = 1;
+angles = zeros(1, numel(points));
+for alpha = points
+
+  
+    %Generate point which are displaced 0.1radians from the initial point
+    %and calculate the slope between these two. That is the derivative in
+    %our point.
+    [ point1x, point1y ] = getCirclePoint(radius, alpha-0.1, center);
+    [ point2x, point2y ] = getCirclePoint(radius, alpha+0.1, center);
+    slope = (point2y-point1y)/(point2x-point1x);
+    angles(index) = atan(slope);
+    disp(angles(index));
+    
+    %Set index
+    index = index +1;
+end
+
+gcf;
 plot(x, y, 'yp', 'MarkerSize', 25, 'MarkerFaceColor', [1 204/255 0]);
 
 set(gca, 'color', [0 51/255 153/255]);
 
-axis([-3*radius 3*radius -radius 3*radius ]);
+%axis([-3*radius 3*radius -radius 3*radius ]);
 axis equal
 hold off;
-pause(0.001);
-end
 
 end
 
