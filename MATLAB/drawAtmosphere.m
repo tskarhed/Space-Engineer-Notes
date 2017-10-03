cda3ec4760289f563aa70b2f888d2ce277a28568
Tobias Skarhed <dehrax@users.noreply.github.com>
2017-10-03 09:24:52 +0200

    Add comments

---
+++ MATLAB/drawAtmosphere.m
@@ -0,0 +1,17 @@
+function drawAtmosphere( input_args )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+%Troposphere 12000m
+
+%Stratosphere 45000m
+
+%Mesosphere 85000m Gradient starts here
+
+%Themosphere 200000m Stars
+
+%Exospehere 500000m Sattelites
+
+end
+
