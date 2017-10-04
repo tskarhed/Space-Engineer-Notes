9ffa6c2c6719e84616b9627e9ff3d3412cc3e3b2
Tobias Skarhed <dehrax@users.noreply.github.com>
2017-10-04 11:37:54 +0200

    Wrap to interval [0, 2*pi]
    Any number can now be used as input.

--- MATLAB/bikewheel.m
+++ MATLAB/bikewheel.m
@@ -8,8 +8,9 @@ center = [radius radius];
 drawCircle(radius, center);
 hold on;
 
-points = linspace(0, 2*pi);
-
+points = randi(100, 1, 20);
+%points = linspace(0, 2*pi);
+points
 [x, y] = getCirclePoint(radius, points, center);
 
 index = 1;
@@ -17,18 +18,24 @@ angles = zeros(1, numel(points));
 slopes = zeros(1, numel(points));
 for alpha = points
 
-  
     %Generate point which are displaced 0.1radians from the initial point
     %and calculate the slope between these two. That is the derivative in
     %our point.
     [ point1x, point1y ] = getCirclePoint(radius, alpha-0.1, center);
     [ point2x, point2y ] = getCirclePoint(radius, alpha+0.1, center);
     slope = (point2y-point1y)/(point2x-point1x);
+
     slopes(index) = slope;
     
     %-pi for angles gives us a positive angle in the opposite direction. It
     %works ;)
-    if (pi < alpha) && (alpha < 2*pi)
+    
+    %Redistribute alpha to get in the interval [0, 2*pi]
+    redAlpha = wrapTo2Pi(alpha);
+    alpha
+    redAlpha
+    
+    if (pi < redAlpha) && (redAlpha < 2*pi)
        angles(index) = atan(slope)-pi;
     else
        angles(index) = atan(slope);
