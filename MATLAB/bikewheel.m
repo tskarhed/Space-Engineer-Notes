e7726d3c6f9725444346eae830ef71fc8b122a95
Tobias Skarhed <dehrax@users.noreply.github.com>
2017-10-10 16:40:17 +0200

    Write animation to video file

--- MATLAB/bikewheel.m
+++ MATLAB/bikewheel.m
@@ -3,34 +3,44 @@ function bikewheel(radius, v, angle)
 %assignment
 %Detailed explanation goes here
 
+writer = VideoWriter('flag.avi');
+
+open(writer);
 center = [0 0];
 
 %drawCircle(radius, center);
-hold on;
-
-%points = randi(100, 1, 20);
-points = linspace(0, 2*pi, 12);
-%points
-[x, y] = getCirclePoint(radius, points, center);
 
 
-
-
-%figure(2);
-%plot(linspace(0,100, numel(slopes)), angles);
+for i=linspace(0,2*pi, 100)
 
 gcf;
+points = linspace(i-2*pi, i, 12);
+[x, y] = getCirclePoint(radius, points, center);
+
 plot(x, y, 'yp', 'MarkerSize', 25, 'MarkerFaceColor', [1 204/255 0]);
 
 set(gca, 'color', [0 51/255 153/255]);
 
 axis([-3*radius 3*radius -radius 3*radius ]);
 axis equal
+
+
+
+writeVideo(writer, getframe);
+  pause(0.001);
+end
+%points = randi(100, 1, 20);
+%points = linspace(0, 2*pi, 12);
+%points
+
+
+%figure(2);
+%plot(linspace(0,100, numel(slopes)), angles);
+
 %title('Cykelstänk', 'fontsize', 20);
 %xlabel('Längd [m]', 'fontsize', 18);
 %ylabel('Höjd [m]', 'fontsize', 18);
-hold off;
-
+close(writer);
 disp('Done!');
 
 end
